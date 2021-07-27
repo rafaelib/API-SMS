15777803ae53e02355f4920360d994126233098d
thiagoribb <thiribeiro142@gmail.com>
2021-07-27 15:45:32 -0300

    preparando review

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import "./setup";
-import app from "./app";
-
-const port: number = Number(process.env.PORT);
-
-app.listen(port, () => {
-  console.log(`Server is listening on port 4000.`);
-});
--- src/validations/nameValidation.ts
+++
@@ -1,7 +0,0 @@
-function nameValidation(str :string){
-    if (typeof(str)!== "string" || str.length < 1) return false
-
-    return true;
-}  
-
-export default nameValidation;
